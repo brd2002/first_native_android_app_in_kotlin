2e6e93fc6292c62a940543f77da10076dcfde6ca
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-01-02 21:25:15 +0530

    complete first app first lecture

--- app/src/main/java/com/example/my_first_native_android_project/MainActivity.kt
+++ app/src/main/java/com/example/my_first_native_android_project/MainActivity.kt
@@ -2,24 +2,19 @@ package com.example.my_first_native_android_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.my_first_native_android_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    // fuction / method ->
+    // using the same function with more
+    // its connect xml with the kotlin and xml convert in java file
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-    }
-    // no output and no input
-    fun print(){
-        println("Hello world")
-    }
-    // tekes input but not return anything
-    fun print(text :String){
-        println("hello world" + text)
-    }
-    fun sumOfNNumbers(num : Int) : Int{
-        return num*(num+1)/2;
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        // connect xml code with kotlin code
+        binding.textTitle.text = "App of tomorrow"
     }
 }
