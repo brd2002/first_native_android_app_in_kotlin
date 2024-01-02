dbec9838202577ed7c29ae15d39792612e2d6aa3
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-01-02 20:43:09 +0530

    this is the first commit

--- app/src/main/java/com/example/my_first_native_android_project/MainActivity.kt
+++ app/src/main/java/com/example/my_first_native_android_project/MainActivity.kt
@@ -4,10 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    // fuction / method ->
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+    }
+    // no output and no input
+    fun print(){
+        println("Hello world")
+    }
+    // tekes input but not return anything
+    fun print(text :String){
+        println("hello world" + text)
+    }
+    fun sumOfNNumbers(num : Int) : Int{
+        return num*(num+1)/2;
     }
 }
